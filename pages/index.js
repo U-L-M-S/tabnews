b77d76836b6d24ab0759fb688eb455db7a0424b5
U-L-M-S <uinclis@outlook.de>
2023-08-11 17:37:33 +0200

    add prettier and config it

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>If you can read this I hope u have a amazing day 😁</h1>
+    return <h1>If you can read this I hope u have a amazing day 😁</h1>;
 }
 
 export default Home;
