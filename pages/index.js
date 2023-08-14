a6a6d3cd9571a8690fe7e2f082c60508f7778a93
U-L-M-S <uinclis@outlook.de>
2023-08-14 13:19:30 +0200

    Thanks

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>If you can read this I hope u have a amazing day 😁</h1>;
+    return <h1>If you can read this I hope u have a amazing day 😁\n</h1>;
+    return <p> Thank you Kookie and Mahal for being the first persons to visite my website :3 </p>
 }
 
 export default Home;
