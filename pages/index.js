a0cdb88d4d12d874beea6134cd47a52d640015b4
U-L-M-S <uinclis@outlook.de>
2023-08-14 13:59:37 +0200

    Fix Break Line Bug the <br> syntax was creating a bug

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>If you can read this I hope u have a amazing day 😁 Thank you Kookie and Mahal for being the first persons to visite my website :3</h1>
+    return <h1>If you can read this I hope u have a amazing day 😁 Thank you Mahal❤️  and Kookie🍪 for being the first persons to visite my website :3</h1>
 }
 
 export default Home;
