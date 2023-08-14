68b5bd9e5421dc0108cc8e443bcfdbefc0ff0d7e
U-L-M-S <uinclis@outlook.de>
2023-08-14 13:52:59 +0200

    fix <p> bug AGAIN

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <p>If you can read this I hope u have a amazing day 😁<br> Thank you Kookie and Mahal for being the first persons to visite my website :3</p>;
+    return <p>If you can read this I hope u have a amazing day 😁<br> Thank you Kookie and Mahal for being the first persons to visite my website :3</p>
 }
 
 export default Home;
