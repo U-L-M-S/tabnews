4e5ad6db558014bfb89f837a9b0f9897578afc3f
U-L-M-S <uinclis@outlook.de>
2023-08-14 13:56:17 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <p>If you can read this I hope u have a amazing day 😁<br> Thank you Kookie and Mahal for being the first persons to visite my website :3</p>
+    return <h1>If you can read this I hope u have a amazing day 😁 Thank you Kookie and Mahal for being the first persons to visite my website :3</h1>
 }
 
 export default Home;
